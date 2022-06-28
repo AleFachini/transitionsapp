fee05f028e1bba00d17b8585941a7ee258b12d23
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-06-28 19:56:51 -0300

    Varias transiciones de pagina

--- lib/src/pages/pagina1_page.dart
+++ lib/src/pages/pagina1_page.dart
@@ -25,17 +25,33 @@ class PaginaOnePage extends StatelessWidget {
     return PageRouteBuilder(
       pageBuilder: (BuildContext context, Animation<double> animation,
           Animation<double> secondaryAnimation) {
-        return PaginaTwoPage();
+        return PaginaTwoPage(); //este es el child que usa transitionBuilder
       },
       //transitionDuration: Duration(seconds: 2),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         final curvedAnimation =
             CurvedAnimation(parent: animation, curve: Curves.easeInOut);
-        return SlideTransition(
-          //Position sirve para determinar por donde entra o sale la pagina
-          position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero)
-              .animate(curvedAnimation),
-          child: PaginaTwoPage(),
+        // return SlideTransition(
+        //   //Position sirve para determinar por donde entra o sale la pagina
+        //   position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero)
+        //       .animate(curvedAnimation),
+        //   child: PaginaTwoPage(),
+        // );
+
+        // return ScaleTransition(
+        //   scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+        //   child: PaginaTwoPage(),
+        // );
+
+        // return RotationTransition(
+        //   //Queda mejor con duration de la curvedAnimation
+        //   turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+        //   child: child,
+        // );
+
+        return FadeTransition(
+          child: child,
+          opacity: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
         );
       },
     );
